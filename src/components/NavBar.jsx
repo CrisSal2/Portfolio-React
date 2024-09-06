16354ff0bca3cfe89118f1b07170e850c0042f1f
CrisSal2 <salgado.chris.m@gmail.com>
2024-09-05 17:35:38 -0700

    added navigation to navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,10 @@
-
+import React from 'react'
+import {Link} from 'react-router-dom'
 
 const data = [
     {
         label: 'HOME',
-        to: '/home'
+        to: '/'
     },
     {
         label: 'SKILLS',
@@ -32,14 +33,47 @@ function NavBar() {
     return (
         <nav className="navbar">
             <div className='navbar_container'>
+                <Link to={'/'}>
                 <button>Home</button>
+                </Link>
+                <Link to={'/skills'}>
                 <button>Skills</button>
+                </Link>
+                <Link to={'/projects'}>
                 <button>Projects</button>
+                </Link>
+                <Link to={'/contact'}>
                 <button>Contact</button>
+                </Link>
+                <Link to={'/resume'}>
                 <button>Resume</button>
+                </Link>
             </div>
         </nav>
     );
 };
 
 export default NavBar;
+
+
+
+
+
+
+/* 
+
+<nav className="navbar">
+            <div className='navbar_container'>
+                <ul>
+                    {
+                    data.map((item, key)=> {
+                        <li key={key} className='buttonItem'>
+                            <Link className='buttonItemLink' to={item.to}>
+                            {item.label}
+                            </Link>
+                        </li>
+                    })
+                    }
+                </ul>
+            </div>
+        </nav> */

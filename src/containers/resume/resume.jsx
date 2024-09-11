145d0292e5c776260b0f56b94364cd65e31ed1a7
CrisSal2 <salgado.chris.m@gmail.com>
2024-09-11 00:05:38 -0700

    added resume section

--- src/containers/resume/resume.jsx
+++ src/containers/resume/resume.jsx
@@ -1,11 +1,18 @@
-
+import comingSoon from '../../assets/images/coming-soon.png';
+import './resume.css';
 
 function Resume() {
-    return(
-        <div>
-        Resume Page
-        </div>
-    )
+  return (
+    <div className="resume-download-container">
+      <h2>Download My Resume</h2>
+      <div className='image-container'>
+        <img src={comingSoon} alt="coming-soon" />
+      </div>
+      <a href="/resume.pdf" download="ChristianSalgado_Resume.pdf" className="download-btn">
+        Download Resume
+      </a>
+    </div>
+  );
 }
 
 export default Resume;
